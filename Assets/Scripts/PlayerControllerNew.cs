27a806d1a9607516dc43e9c2e54edd1f771547a1
TARTIGA <tima_ig@mail.ru>
2019-06-26 19:06:23 +0300

    new Web version

--- Assets/Scripts/PlayerControllerNew.cs
+++ Assets/Scripts/PlayerControllerNew.cs
@@ -8,8 +8,10 @@ public class PlayerControllerNew : MonoBehaviour
     public float speed = 10;
     public Text countText;
     public Text winText;
+    public Text gameOverText;
     private Rigidbody2D rb2d;
     private int count;
+    private bool gameOver;
 
     private int winCount = 10;
 
@@ -20,20 +22,26 @@ public class PlayerControllerNew : MonoBehaviour
     /// </summary>
     void Start()
     {
+        gameOver = false;
         rb2d = GetComponent<Rigidbody2D>();
         count = 0;
         winText.text = "";
-        SetCountText();
+        gameOverText.gameObject.SetActive(false);
+        SetText();
     }
     /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
     /// </summary>
     void FixedUpdate()
     {
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+        if (!gameOver)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+            rb2d.AddForce(movement * speed);
+        }
     }
 
     /// <summary>
@@ -47,11 +55,25 @@ public class PlayerControllerNew : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-            SetCountText();
+            SetText();
         }
     }
 
-    void SetCountText()
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Border"))
+        {
+            gameOver = true;
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+
+    void SetText()
     {
         countText.text = "Count: " + count.ToString();
         if (count >= winCount)
